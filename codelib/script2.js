37ece6e518855073b0b9371b0ec8bdd2a5a5a13c
anglcode <adlaird6471@gmail.com>
2025-10-02 10:17:11 -0700

    Add files via upload

--- codelib/script2.js
+++ codelib/script2.js
@@ -23,6 +23,8 @@ document.addEventListener('DOMContentLoaded',() =>{
                 if(filesread==files.length){
                     if(refrencesong && songlist.length>0){
                         sortbybpm(refrencesong,songlist);
+                        sortbyart(refrencesong,songlist);
+                        sortbygen(refrencesong,songlist);
                     }else{
                         console.error("missing refsong or list");
                     }
@@ -50,5 +52,27 @@ function sortbybpm(ref,songs){
         const paratext = document.createElement('p');
         paratext.textContent = `${song.title} (${song.bpm}bpm) - dif:${song.bpmdif}`;
         songhold.appendChild(paratext);
+    });
+}
+
+function sortbyart(ref,songs){
+    const refart = ref.artist;
+    const sorted = songs.map(song => ({
+    ...song,artdif: song.artist == refart ? true:false
+    })).sort((a,b) => b.artdif - a.artdif);
+    console.log(`Target Artist ${refart}`);
+    console.log(`Sorted by Artist Match`);
+    sorted.forEach(song =>{
+        console.log(`${song.title} (${song.artist}) - target:${song.artdif}`);
+    });
+}
+
+function sortbygen(ref,songs){
+    const refgen = ref.genres;
+    const sorted = songs.map(song =>({
+       ...song,genmat: song.genres.filter((gen) => refgen.some(ref => gen.includes(ref))) 
+    })).sort((a,b) => b.genmat.length - a.genmat.length);
+    sorted.forEach(song => {
+        console.log(`${song.title} (${song.genres}) - matches:${song.genmat}`);
     });
 }
